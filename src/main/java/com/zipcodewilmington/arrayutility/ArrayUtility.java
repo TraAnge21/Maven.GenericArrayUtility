2c02f8bd49b9edc8d0d1efde6da7dd249e0739b5
That is me <trab.die@gmail.com>
2021-12-01 01:29:58 -0500

    generics

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,53 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Function;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+//public class ArrayUtility {
+
+public class ArrayUtility<SomeType> {
+//        private final SomeType[] array;
+
+    private final SomeType[] array;
+
+    public ArrayUtility(SomeType[] array) {
+
+        this.array = array;
+    }
+
+    public SomeType findOddOccurringValue() {
+
+        return null;
+
+    }
+
+    public SomeType GetMostCommonFromMerge() {
+
+        return null;
+
+    }
+
+    public Integer getNumberOfOccurrences(SomeType valueToEvaluate) {
+        int countOccurrence =0;
+        for ( int j =0 ; j <= array.length -1; j++) {
+            if ( array[j]==(valueToEvaluate)){
+                countOccurrence++;
+            }
+        }
+        return countOccurrence;
+    }
+
+    public SomeType[] filter(Function<SomeType, Boolean> predicate) {
+
+
+
+        return null;
+
+    }
 }
+
+
